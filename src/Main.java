9f166a5b4c4507a4b1e60f3f161ac3860113c7c6
zakhaMain <zakharwebkrot@gmail.com>
2024-11-23 12:38:34 +0300

    homework n2 23.11

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
-    public static void main(String[] args){
+    public static void onehm(String[] args){
         for (int i = 1; i <=10 ; i++) {
             for (int j = 1; j <= 10; j++) {
                 System.out.printf(i * j + " ");
             }
+            System.out.println();
         }
         }
     }
--- src/two3hm.java
+++ src/two3hm.java
@@ -1,7 +1,6 @@
 public class two3hm {
     public static void main(String[] args) {
         String[] words = {"a", "b", "c", "d", null, "f", "g", "lmao"};
-        System.out.println("\nfor");
         for (int i = 0; i < words.length; i += 2) {
             if (words[i] == null) {
                 System.out.print("\n Ошибка!\n");
